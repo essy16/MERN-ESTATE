8960f320b3f293d889e5d57133ca7a602945bd75
essy16 <essyc14@gmail.com>
2025-04-22 12:35:50 +0300

    MONGO_DB-CONNECTED

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGOOSE_URL)
+  .then(() => {
+    console.log("Momgo is runnnning");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
